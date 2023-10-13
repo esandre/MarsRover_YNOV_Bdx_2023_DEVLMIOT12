422d3c24a7dcdf2d02c477e46d5c5e67b2824fbc
Enzo Sandré <atelier@enzosandre.fr>
2023-10-13 13:54:33 +0200

    Avancement vers le Nord

---
+++ MarsRover.Test/MouvementTest.cs
@@ -0,0 +1,20 @@
+﻿namespace MarsRover.Test;
+
+public class MouvementTest
+{
+    [Fact]
+    public void AvancerNord()
+    {
+        // ETANT DONNE un rover orienté nord
+        var rover = new Rover(PointCardinal.Nord);
+
+        // QUAND on avance
+        var étatFinal = rover.Avancer();
+
+        // ALORS la latitude s'incrémente
+        Assert.Equal(rover.Latitude + 1, étatFinal.Latitude);
+
+        // ET l'orientation reste identique
+        Assert.Equal(étatFinal.Orientation, rover.Orientation);
+    }
+}
--- MarsRover/Rover.cs
+++ MarsRover/Rover.cs
@@ -3,12 +3,16 @@
 public class Rover
 {
     public PointCardinal Orientation { get; }
+    public uint Latitude { get; }
 
-    public Rover(PointCardinal orientationDépart)
+    public Rover(PointCardinal orientationDépart, uint latitude = 0)
     {
         Orientation = orientationDépart;
+        Latitude = latitude;
     }
 
     public Rover TournerADroite() => new(Orientation.SuivantHoraire);
     public Rover TournerAGauche() => new(Orientation.SuivantAntihoraire);
+
+    public Rover Avancer() => new (Orientation, Latitude + 1U);
 }
