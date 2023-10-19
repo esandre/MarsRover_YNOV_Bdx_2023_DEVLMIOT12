c1a51614bb1e3908ec12b8d81c0f6bb64f433bdd
Enzo Sandré <atelier@enzosandre.fr>
2023-10-19 14:31:57 +0200

    [RED] Solution abandonnée sur les commandes

---
+++ MarsRover.Test/CommandeTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FsCheck.Xunit;
+using MarsRover.Test.Utilities;
+
+namespace MarsRover.Test;
+
+public class CommandeTest
+{
+    [Theory]
+    [InlineData(Commandes.Avancer, nameof(Rover.Avancer))]
+    [InlineData(Commandes.Droite, nameof(Rover.TournerADroite))]
+    [InlineData(Commandes.Gauche, nameof(Rover.TournerAGauche))]
+    [InlineData(Commandes.Reculer, nameof(Rover.Reculer))]
+    public void CommandeEtActionDonnentLeMêmeRésultat(char commande, string nameOfMethodToCall)
+    {
+        // ETANT DONNE une commande d'un caractère <commande>
+        var command = Commandes.Interpréter(commande);
+
+        // QUAND on applique cette commande sur le rover
+        var rover = RoverBuilder.Default;
+        var résultatAvecCommande = command.ExécuterSur(rover);
+
+        // ALORS le résultat est le même que si on avait appelé la méthode <nameOfMethodToCall>
+        var roverTémoin = RoverBuilder.Default;
+        var résultatAttendu = typeof(Rover).GetMethod(nameOfMethodToCall)!.Invoke(roverTémoin, Array.Empty<object>());
+
+        Assert.Equal(résultatAttendu, résultatAvecCommande);
+    }
+}
---
+++ MarsRover/Commandes.cs
@@ -0,0 +1,54 @@
+﻿namespace MarsRover;
+
+public static class Commandes
+{
+    public const char Avancer = 'A';
+    public const char Reculer = 'R';
+    public const char Droite = 'D';
+    public const char Gauche = 'G';
+
+    public static ICommande<Rover> Interpréter(char commande)
+        => commande switch
+           {
+               Avancer => new CommandeAvancer(),
+               Reculer => new CommandeReculer(),
+               Droite  => new CommandeTournerADroite(),
+               Gauche  => new CommandeTournerADroite(),
+               _       => new CommandeIdempotente()
+           };
+}
+
+public interface ICommande<T>
+{
+    T ExécuterSur(T objet);
+}
+
+public class CommandeIdempotente : ICommande<Rover>
+{
+    /// <inheritdoc />
+    public Rover ExécuterSur(Rover objet) => objet;
+}
+
+public class CommandeAvancer : ICommande<Rover>
+{
+    /// <inheritdoc />
+    public Rover ExécuterSur(Rover objet) => objet.Avancer();
+}
+
+public class CommandeReculer : ICommande<Rover>
+{
+    /// <inheritdoc />
+    public Rover ExécuterSur(Rover objet) => objet.Reculer();
+}
+
+public class CommandeTournerADroite : ICommande<Rover>
+{
+    /// <inheritdoc />
+    public Rover ExécuterSur(Rover objet) => objet.TournerADroite();
+}
+
+public class CommandeTournerAGauche : ICommande<Rover>
+{
+    /// <inheritdoc />
+    public Rover ExécuterSur(Rover objet) => objet.TournerAGauche();
+}
